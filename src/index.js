cdc7dcd16adde01c5d2fa02fd827181494535fa6
KavinAravindhan <kavin.aravindhan@gmail.com>
2022-08-31 20:21:49 +0530

    Landing Page Edit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { GlobalStyles } from './global-styles';
 import App from './app';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+render(<>
+        <GlobalStyles />
+        <App />
+        </>,
+    document.getElementById('root')
+);
 
