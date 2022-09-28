3ec4b255d4e49d5897ba94b1afe5d1dafaa81060
KavinAravindhan <kavin.aravindhan@gmail.com>
2022-09-28 13:13:17 +0530

    header done & firebase initiated

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,14 @@
 import React from 'react';
-// import { Feature, OptForm } from '../components';
+import { Feature, OptForm } from '../components';
 import { HeaderContainer } from '../container/header';
 import { JumbotronContainer } from '../container/jumbotron';
 import { FaqsContainer } from '../container/faqs';
 import { FooterContainer } from '../container/footer';
 
-export default function Home() 
-{
+export default function Home() {
     return (
         <>
-        {/* <HeaderContainer>
+        <HeaderContainer>
             <Feature>
             <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
             <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
@@ -20,13 +19,11 @@ export default function Home()
                 <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
             </OptForm>
             </Feature>
-        </HeaderContainer> */}
-
-        <HeaderContainer>
-            <JumbotronContainer />
-            <FaqsContainer />
-            <FooterContainer />
         </HeaderContainer>
-    </>
+
+        <JumbotronContainer />
+        <FaqsContainer />
+        <FooterContainer />
+        </>
     );
 }
