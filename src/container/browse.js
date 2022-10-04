83e4ed1e7db5519c74510499a61bf785364872d2
KavinAravindhan <kavin.aravindhan@gmail.com>
2022-10-05 01:10:09 +0530

    profile setting

--- src/container/browse.js
+++ src/container/browse.js
@@ -1,11 +1,28 @@
-import React, { useContext } from 'react';
-import { SelectProfileContainer } from './profiles';
+import React, { useState, useEffect, useContext } from 'react';
+import Fuse from 'fuse.js';
+import { Card, Header, Loading, Player } from '../components';
+import * as ROUTES from '../constants/routes';
+import logo from '../logo.svg';
 import { FirebaseContext } from '../context/firebase';
+import { SelectProfileContainer } from './profiles';
+import { FooterContainer } from './footer';
 
 export function BrowseContainer({ slides })
 {
-    const { firebase } = useContext(FirebaseContext);
-    const user = firebase.auth.currentUser || {};
+    const [category, setCategory] = useState('series');
+    const [profile, setProfile] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [slideRows, setSlideRows] = useState([]);
 
-    return <SelectProfileContainer user={user} />;
+    const { firebase } = useContext(FirebaseContext);
+    const user = firebase.auth().currentUser || {};
+
+    useEffect(() => {
+        setTimeout(() => {
+        setLoading(false);
+        }, 3000);
+    }, [profile.displayName]);
+
+    return <SelectProfileContainer user={user} setProfile={setProfile} />;
 }
